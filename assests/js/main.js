6adba0daa464783d158c712a5b5a20b00c82090c
Tanuja Gariya <gariyatanu@353@gmail.com>
2025-03-28 11:42:35 +0530

    headerand footer

---
+++ assests/js/main.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+         const mobileNav = document.querySelector('.mobile-nav');
+         mobileNav.style.display = (mobileNav.style.display === "block") ? "none" : "block";
+     }
